f3560f7c6c0db39a9cd5aac9006b212923024d8e
victorbaptistalemos <victorbaptistalemos@icloud.com>
2021-09-22 17:33:24 +0000

    Database methods created

--- contacts/database.py
+++ contacts/database.py
@@ -1,14 +1,17 @@
-from sqlite3 import connect, OperationalError
+from abc import ABC
+from sqlite3 import connect, Cursor, OperationalError
+from typing import Optional
 
 
-class Database:
+class Database(ABC):
     __database: str = './contacts/.contacts.s3db'
+    __ADD: str = 'INSERT INTO contacts_list VALUES(NULL,?, ?,?)'
+    __DELETE: str = 'DELETE FROM contacts_list WHERE name = ?'
+    __UPDATE: str = 'UPDATE contacts_list SET number=? WHERE number =? AND name =?'
 
     @classmethod
     def execute(cls, query: str, parameters: tuple) -> bool:
         with connect(cls.__database) as database:
-            print(database)
-            print('You have successfully connected to the Database')
             cursor = database.cursor()
             try:
                 cursor.execute(query, parameters)
@@ -16,3 +19,27 @@ class Database:
                 return True
             except OperationalError:
                 return False
+
+    @classmethod
+    def list(cls) -> Optional[list]:
+        with connect(cls.__database) as database:
+            cursor: Cursor = database.cursor()
+            query: str = 'SELECT * FROM contacts_list ORDER BY name desc'
+            try:
+                cursor.execute(query)
+                query: list = cursor.fetchall()
+                return query
+            except OperationalError:
+                return None
+
+    @classmethod
+    def add(cls) -> str:
+        return cls.__ADD
+
+    @classmethod
+    def delete(cls) -> str:
+        return cls.__DELETE
+
+    @classmethod
+    def update(cls) -> str:
+        return cls.__UPDATE
