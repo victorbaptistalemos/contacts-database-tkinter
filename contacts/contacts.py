ecb5f05421c36df9009b41421c6817531a04d631
victorbaptistalemos <victorbaptistalemos@icloud.com>
2021-09-22 16:53:11 +0000

    Scrollbars added. close #5

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,9 +1,9 @@
-from tkinter import CENTER, E, PhotoImage, Tk, W
+from tkinter import CENTER, E, Label, PhotoImage, Tk, W
 from tkinter.ttk import Button as TButton
 from tkinter.ttk import Entry as TEntry
 from tkinter.ttk import Label as TLabel
 from tkinter.ttk import LabelFrame as TLabelFrame
-from tkinter.ttk import Style, Treeview
+from tkinter.ttk import Scrollbar, Style, Treeview
 
 
 class Contacts:
@@ -13,21 +13,22 @@ class Contacts:
         self.root: Tk = root
         style: Style = Style()
         style.configure('.', font='TimesNewRoman 12', background='black', foreground='violet')
-        style.configure('TLabelframe.Label', font='TimesNewRoman 14 bold')
-        style.configure('TLabel', font='TimesNewRoman 14')
-        style.configure('TEntry', fieldbackground='violet', foreground='black')
         style.configure('TButton', font='TimesNewRoman 14 bold')
+        style.configure('TEntry', fieldbackground='violet', foreground='black')
+        style.configure('TLabel', font='TimesNewRoman 14')
+        style.configure('TLabelframe.Label', font='TimesNewRoman 14 bold')
+        style.configure('TScrollbar', background='violet')
+        style.configure('Treeview', fieldbackground='black')
+        style.configure('Treeview.Heading', font='TimesNewRoman 12 bold')
         style.map(
             'TButton',
             background=[('active', 'purple'), ('!active', 'violet')],
             foreground=[('active', 'black'), ('!active', 'black')])
-        style.configure('Treeview', fieldbackground='black')
         style.map(
             'Treeview',
             foreground=[('selected', 'black'), ('!selected', 'violet')],
             background=[('selected', 'violet'), ('!selected', 'black')],
         )
-        style.configure('Treeview.Heading', font='TimesNewRoman 12 bold')
         style.map(
             'Treeview.Heading',
             background=[('active', 'violet'), ('!active', 'violet')],
@@ -43,7 +44,7 @@ class Contacts:
 
     def top_frame(self) -> None:
         top_frame = TLabelFrame(self.root, text='Criar novo contato', labelanchor='n')
-        top_frame.place(x=25, y=10, width=400, height=150)
+        top_frame.place(x=20, y=10, width=400, height=150)
         TLabel(top_frame, text='Nome:').place(x=75, y=10, anchor=E)
         TLabel(top_frame, text='Email:').place(x=75, y=35, anchor=E)
         TLabel(top_frame, text='Número:').place(x=75, y=60, anchor=E)
@@ -57,13 +58,16 @@ class Contacts:
         icon: PhotoImage = PhotoImage(file='contacts/.contacts.gif')
         label: TLabel = TLabel(image=icon, border=0)
         label.image = icon
-        label.place(x=450, y=35)
+        label.place(x=455, y=35)
 
     def bottom_frame(self):
         bottom_frame = Treeview(columns=(0, 0))
-        bottom_frame.place(x=25, y=170, width=550, height=250)
+        bottom_frame.place(x=20, y=170, width=550, height=250)
         bottom_frame.heading('#0', text='Nome', anchor=W)
         bottom_frame.heading('#1', text='Email', anchor=W)
         bottom_frame.heading('#2', text='Número', anchor=W)
-        TButton(self.root, text='Modificar contato').place(x=175, y=450, anchor=CENTER, width=200)
-        TButton(self.root, text='Deletar contato').place(x=425, y=450, anchor=CENTER, width=200)
+        Scrollbar(orient='vertical', command=bottom_frame.yview).place(x=570, y=170, height=250, width=15)
+        Scrollbar(orient='horizontal', command=bottom_frame.xview).place(x=20, y=420, height=15, width=550)
+        Label(self.root, background='violet').place(x=570, y=420, height=15, width=15)
+        TButton(self.root, text='Modificar contato').place(x=175, y=465, anchor=CENTER, width=200)
+        TButton(self.root, text='Deletar contato').place(x=420, y=465, anchor=CENTER, width=200)
