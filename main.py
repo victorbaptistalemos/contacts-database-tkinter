bf7009e7aa3c217c586e9794e4667863e5aacaed
victorbaptistalemos <victorbaptistalemos@icloud.com>
2021-09-23 12:35:10 +0000

    Icon added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from contacts.contacts import Contacts
-from tkinter import Tk
+from tkinter import PhotoImage, Tk
 
-root = Tk()
+root = Tk(className="Minha Agenda de Contatos")
+root.tk.call('wm', 'iconphoto', root.__getattribute__('_w'), PhotoImage(file='./contacts/.logo.gif'))
 root.title('Minha Agenda de Contatos')
 root.geometry("600x500+450+200")
 root.resizable(False, False)
